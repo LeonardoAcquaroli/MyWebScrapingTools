6ddaa395ad96476c5322354cbb5072f545b42916
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2023-09-27 12:08:35 +0200

    Create init_driver.py

---
+++ init_driver.py
@@ -0,0 +1,8 @@
+import importlib.machinery
+import requests
+# Fetch the raw content of the .py file.
+response = requests.get('https://raw.githubusercontent.com/LeonardoAcquaroli/MyWebScrapingTools/main/MyWebscrapingTools.py')
+response.raise_for_status()
+# Use importlib to import the .py file as a module.
+loader = importlib.machinery.SourceFileLoader('module_name', 'MyWebscrapingTools.py')
+module = loader.load_module()
