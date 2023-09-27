178f394b1561da224e598d88f21ab38ca2e715cf
Leonardo Acquaroli <89029338+LeonardoAcquaroli@users.noreply.github.com>
2023-09-27 12:38:34 +0200

    Update import_MyWebScrapingTools.py
    changed module_name into MyWsTools

--- import_MyWebScrapingTools.py
+++ import_MyWebScrapingTools.py
@@ -5,9 +5,10 @@ def import_MyWebScrapingTools():
   response = requests.get('https://raw.githubusercontent.com/LeonardoAcquaroli/MyWebScrapingTools/main/MyWebscrapingTools.py')
   response.raise_for_status()
   # Use importlib to import the .py file as a module.
-  loader = importlib.machinery.SourceFileLoader('module_name', 'MyWebscrapingTools.py')
+  loader = importlib.machinery.SourceFileLoader('MyWsTools', 'MyWebscrapingTools.py')
   module = loader.load_module()
   return module
+  
 if __name__ == "__main__":
   print('prova')
   import_MyWebScrapingTools()
