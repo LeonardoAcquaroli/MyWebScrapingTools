1fcee443c4aab1ffdd4b22c0edc94447b4209a60
Leonardo Acquaroli <89029338+LeonardoAcquaroli@users.noreply.github.com>
2023-10-02 12:12:21 +0200

    Add files via upload

--- import_MyWebScrapingTools.py
+++ import_MyWebScrapingTools.py
@@ -2,10 +2,12 @@ def import_MyWebScrapingTools():
   import importlib.machinery
   import requests
   # Fetch the raw content of the .py file.
-  response = requests.get('https://raw.githubusercontent.com/LeonardoAcquaroli/MyWebScrapingTools/main/MyWebscrapingTools.py')
+  GitHub_url = 'https://raw.githubusercontent.com/LeonardoAcquaroli/MyWebScrapingTools/main/MyWebscrapingTools.py'
+  response = requests.get(GitHub_url)
   response.raise_for_status()
   # Use importlib to import the .py file as a module.
-  loader = importlib.machinery.SourceFileLoader('MyWsTools', 'MyWebscrapingTools.py')
+  module_name = 'MyWsTools'
+  loader = importlib.machinery.SourceFileLoader(module_name, 'MyWebscrapingTools.py')
   module = loader.load_module()
   return module
   
