88498b9ea19e28f851ae81130a93a581fb4c4493
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2023-10-12 15:30:55 +0200

    to autoinstall chromedriver

--- import_MyWebScrapingTools.py
+++ import_MyWebScrapingTools.py
@@ -2,11 +2,14 @@ def import_MyWebScrapingTools():
   import importlib.machinery
   import requests
   # Fetch the raw content of the .py file.
-  response = requests.get('https://raw.githubusercontent.com/LeonardoAcquaroli/MyWebScrapingTools/main/MyWebscrapingTools.py')
+  GitHub_url = 'https://raw.githubusercontent.com/LeonardoAcquaroli/MyWebScrapingTools/main/MyWebscrapingTools.py'
+  response = requests.get(GitHub_url)
   response.raise_for_status()
+  # Save the content to a temporary file.
+  with open('MyWebscrapingTools.py', 'wb') as file:
+    file.write(response.content)
   # Use importlib to import the .py file as a module.
-  loader = importlib.machinery.SourceFileLoader('MyWsTools', 'MyWebscrapingTools.py')
+  module_name = 'MyWsTools'
+  loader = importlib.machinery.SourceFileLoader(module_name, 'MyWebscrapingTools.py')
   module = loader.load_module()
   return module
-  
-import_MyWebScrapingTools()
